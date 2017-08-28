27c928e1c74ccc9f28c1391b65daa80ba3c8b54a
Sarah Scheffler <sscheffler@hmc.edu>
2017-08-28 18:42:47 -0400

    IT WORKS

--- bruteforce/sacred_geometry/src/main.rs
+++ bruteforce/sacred_geometry/src/main.rs
@@ -9,31 +9,6 @@
  * http://www.codinghelmet.com/?path=exercises/expression-from-numbers
  */
 
-/*
- * E = set of all valid expressions
- * Q = queue of expressions that have not been expanded yet
- * N = set of input numbers
- * v = target value
- *
- * Goal: Create an expression N->v using all numbers in N to produce value v
- *
- * for all numbers n in N:
- *    Add expression n->n to Q
- *    Add expression n->n to E
- *
- * while Q is not empty and E does not contain N->v:
- *     e = expression dequeued from Q
- *     for each expression f in E:
- *         G = set of expressions obtained by combining e and f
- *         for each expression g in g:
- *             if g is not in E:
- *                 Enqueue g to Q
- *                 Add g to E
- * 
- * if E contains N->v:
- *     print N->v
- */
-
 /* Encoding: N least significant bits are "is the nth number used"?.  The bits above that are the
  * value of the expression.  E.g. if the numbers are [1, 1, 4], then "1+1" is encoded as "2||110",
  * a.k.a. "10110".  1*4 is encoded as "4||101" a.k.a. "100101". 
@@ -46,10 +21,29 @@ use std::slice::Iter;
 
 fn main() {
     // TODO: should be user input
-    let dierolls = vec![2, 3, 5, 5];
+    //let dierolls = vec![5, 2, 3, 5, 5];
+    //let dierolls = vec![1, 1, 1, 1, 1, 1, 1];
+    //let dierolls = vec![1, 1];
+    let dierolls = vec![6,6,5,6,6,4,2,4,5,4,6,2];
+    //let dierolls = vec![2,4,5,1,4,6,2];
     // Highest sacred prime is 107
-    let target: u8 = 7; // (5 / 5) + (2 * 3)
+    //let target: u8 = 7; // (5 / 5) + (2 * 3)
+    //let target: u8 = 107;
+    let target: u8 = 67;
     let mut solver: Solver = Solver::new(dierolls, target);
+    /*println!("{:b}", solver.expr_to_numbers(0b100001));
+    println!("{:b}", solver.expr_to_numbers(0b110010));
+    println!("{:b}", solver.expr_to_numbers(0b1010100));
+    println!("{:b}", solver.expr_to_numbers(0b1011000));
+    println!("{:b}", solver.expr_to_value(0b100001));
+    println!("{:b}", solver.expr_to_value(0b110010));
+    println!("{:b}", solver.expr_to_value(0b1010100));
+    println!("{:b}", solver.expr_to_value(0b1011000));
+    println!("{:b}", solver.encode_expr(2, 1 << 0));
+    println!("{:b}", solver.encode_expr(3, 1 << 1));
+    println!("{:b}", solver.encode_expr(5, 1 << 2));
+    println!("{:b}", solver.encode_expr(5, 1 << 3));*/
+
     solver.solve();
 }
 
@@ -98,8 +92,8 @@ enum Expression { ExprParts(u32, Operator, u32), DieRoll(u8) }
 impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Expression::ExprParts(lhs, ref op, rhs) => write!(f, "({}){}({})", lhs, op, rhs),
-            Expression::DieRoll(x) => write!(f, "{}", x),
+            Expression::ExprParts(lhs, ref op, rhs) => write!(f, "({:b}){}({:b})", lhs, op, rhs),
+            Expression::DieRoll(x) => write!(f, "[{}]", x),
         }
     }
 }
@@ -107,8 +101,8 @@ impl fmt::Display for Expression {
 impl fmt::Debug for Expression {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Expression::ExprParts(lhs, ref op, rhs) => write!(f, "({}){}({})", lhs, op, rhs),
-            Expression::DieRoll(x) => write!(f, "{}", x),
+            Expression::ExprParts(lhs, ref op, rhs) => write!(f, "({:b}){}({:b})", lhs, op, rhs),
+            Expression::DieRoll(x) => write!(f, "[{}]", x),
         }
     }
 }
@@ -149,7 +143,7 @@ impl Solver {
 
     /** Extract the value from the expression encoding */
     fn expr_to_value(&self, expr: u32) -> u32 {
-        (0xff & (expr >> self.count)) as u32
+        expr >> self.count
     }
 
     /** Extract the numbers used from the expression encoding */
@@ -173,11 +167,13 @@ impl Solver {
     fn solve(&mut self) {
         for i in 0..(self.count as usize) {
             let encoded_num = self.encode_expr(
-                (self.dierolls[i] as u32) << self.count, (1 << i));
+                (self.dierolls[i] as u32), (1 << i));
+            let new_expr = Expression::DieRoll(self.dierolls[i]);
             self.built_exprs.insert(
-                encoded_num, Expression::DieRoll(self.dierolls[i]));
+                encoded_num, new_expr);
             self.queue.push_back(encoded_num);
         }
+
         
         while self.queue.len() > 0 && 
                     !self.built_exprs.contains_key(&self.target_encoded) {
@@ -188,8 +184,8 @@ impl Solver {
             let rhs_possibilities = self.built_exprs.clone();
 
             for &rhs in rhs_possibilities.keys() {
-                let rhs_numbers = self.expr_to_numbers(rhs);
-                let rhs_value = self.expr_to_value(rhs);
+                let rhs_numbers: u32 = self.expr_to_numbers(rhs);
+                let rhs_value: u32 = self.expr_to_value(rhs);
 
                 // Need LHS, RHS to not share any numbers
                 if lhs_numbers & rhs_numbers != 0 {
@@ -199,15 +195,23 @@ impl Solver {
                 for op in Operator::iterator() {
                     let new_value_opt: Option<u32> = match *op {
                         Operator::Add => Some(lhs_value + rhs_value),
-                        Operator::Sub => Some(lhs_value - rhs_value),
-                        Operator::SubReverse => Some(rhs_value - lhs_value),
-                        Operator::Mul => Some(lhs_value * rhs_value),
-                        Operator::Div => if lhs_value % rhs_value == 0 {
+                        Operator::Sub => if lhs_value >= rhs_value {
+                            Some(lhs_value - rhs_value)
+                        } else {
+                            None
+                        },
+                        Operator::SubReverse => if rhs_value >= lhs_value {
+                            Some(rhs_value - lhs_value)
+                        } else {
+                            None
+                        },
+                        Operator::Mul => Some(lhs_value as u32 * rhs_value as u32),
+                        Operator::Div => if (rhs_value != 0) && (lhs_value % rhs_value == 0) {
                             Some(lhs_value / rhs_value)
                         } else {
                             None
                         },
-                        Operator::DivReverse => if rhs_value % lhs_value == 0 {
+                        Operator::DivReverse => if (lhs_value != 0) && (rhs_value % lhs_value == 0) {
                             Some(rhs_value / lhs_value)
                         } else {
                             None
@@ -217,16 +221,15 @@ impl Solver {
                     if new_value_opt.is_none() {
                         continue;
                     }
+
                     let new_value:u32 = new_value_opt.unwrap();
                     let new_numbers:u32 = lhs_numbers | rhs_numbers;
-                    let new_expr = self.encode_expr(new_numbers, new_value);
-                    println!("Got new expr {}", new_expr);
+                    let new_enc = self.encode_expr(new_value, new_numbers);
 
-                    if !self.built_exprs.contains_key(&new_expr) {
-                        println!("Adding the expr {}", new_expr);
-                        self.built_exprs.insert(
-                            new_expr, Expression::ExprParts(lhs, op.clone(), rhs));
-                        self.queue.push_back(new_expr);
+                    if !self.built_exprs.contains_key(&new_enc) {
+                        let new_expr = Expression::ExprParts(lhs, op.clone(), rhs);
+                        self.built_exprs.insert(new_enc, new_expr);
+                        self.queue.push_back(new_enc);
                     }
                 }
 
@@ -235,12 +238,28 @@ impl Solver {
         }
 
         if self.built_exprs.contains_key(&self.target_encoded) {
-            println!("{}", self.built_exprs.get(&self.target_encoded).unwrap_or(&Expression::DieRoll(0)));
+
+            println!("{} = {}", self.target_decoded as u32, self.printer(self.target_encoded));
             //TODO: return
         } else {
             println!("Aww");
             println!("{:?}", self.built_exprs);
         }
+    }
+
+
+    pub fn print_step(&self, expr: Expression) -> String {
+        match expr {
+            Expression::ExprParts(lhs, Operator::SubReverse, rhs) => self.print_step(Expression::ExprParts(rhs, Operator::Sub, lhs)),
+            Expression::ExprParts(lhs, Operator::DivReverse, rhs) => self.print_step(Expression::ExprParts(rhs, Operator::Div, lhs)),
+            Expression::ExprParts(lhs, op, rhs) => format!("({}){}({})", self.printer(lhs), op, self.printer(rhs)),
+            Expression::DieRoll(x) => format!("{}", x),
+        }
+    }
+
+    fn printer(&self, current: u32) -> String {
+        // assumes we have the target
+        self.print_step(self.built_exprs.get(&current).unwrap_or(&Expression::DieRoll(0)).clone())
     }
 }
 
